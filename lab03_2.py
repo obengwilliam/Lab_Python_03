2e0b6c07b92307fd7047d12a96d45f69c8dd3fd1
AITI Student <aiti-student@aiti-090-PC.(none)>
2012-06-21 10:55:27 -0400

    just got done

---
+++ lab03_2.py
@@ -0,0 +1,82 @@
+'''
+creator:obeng william
+description:
+'''
+#
+print 'lab03_2a.py'
+b=1
+i=0
+while i<7:
+    for j in range(1,b):
+          print j,
+    b=b+1
+    print
+    i=i+1
+print "  "
+
+#
+print 'lab03.b.py'
+b=7
+i=0
+while i<6:
+    for j in range(1,b):
+          print j,
+    b=b-1
+    print
+    i=i+1
+
+
+
+print "  "
+
+
+
+
+#
+print 'lab03_2C.PY'
+    
+b=8
+i=0
+for s in range(1,b):
+    for i in range(7-s):
+        print ' ',
+    for j in range(s,1,-1):
+          print j-1,
+    print
+    
+    
+
+print "  "
+
+
+#
+print 'lab03_2d.py'
+b=7
+i=0
+for s in range(1,b):
+    for q in range(0,s-1):
+        print ' ',
+    for j in range(1,b):
+            print j,
+    b=b-1
+    print
+    
+
+    
+print "  "
+
+
+
+print 'lab03_2e.py'
+
+b=8
+i=0
+for s in range(1,b):
+    for i in range(7-s):
+        print ' ',
+    for j in range(s,1,-1):
+          print j-1,
+          
+            
+    print
+    
